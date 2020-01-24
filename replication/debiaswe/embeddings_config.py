4c0973527f70b9f4f8a9b7df1cac49b477101b5b
Kylian van Geijtenbeek <kylian.v.geijtenbeek@hotmail.com>
2020-01-24 13:42:47 +0100

    Finished automatic downloading

---
+++ replication/debiaswe/embeddings_config.py
@@ -0,0 +1,22 @@
+ID = {
+    "word2vec_large" : {
+        "id" : "0",
+        "extension" : ".txt"
+    },
+    "word2vec_small" : {
+        "id" : "0",
+        "extension" : ".txt"
+    },
+    "glove_small" : {
+        "id" : "0",
+        "extension" : ".txt"
+    },
+    "fasttext_large" : {
+        "id" : "0",
+        "extension" : ".txt"
+    },
+    "fasttext_small" : {
+        "id" : "1vW9zYwt4s_i2RMDV4kytGR5wycpV_9vW",
+        "extension" : ".txt"
+    }
+}
